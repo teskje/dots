7d75f7d732402edf4936eb35b2eaddd607fad50f
Jan Teske <jteske@posteo.net>
2025-02-09 16:03:34 +0100

    .

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.color_scheme = 'OneHalfDark'
+config.color_scheme = 'OneHalfDark-fixed'
 
 config.hide_tab_bar_if_only_one_tab = true
 config.default_cursor_style = 'BlinkingBar'
